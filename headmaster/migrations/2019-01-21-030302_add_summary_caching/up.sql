725cbfc793cb0b211030ba9c55a04064dcbe0772
Mike Lubinets <public@mersinvald.me>
2019-01-21 07:02:21 +0300

    migrations for caching feature

---
+++ headmaster/migrations/2019-01-21-030302_add_summary_caching/up.sql
@@ -0,0 +1,8 @@
+-- Your SQL goes here
+CREATE TABLE IF NOT EXISTS summary_cache
+(
+   user_id BIGINT NOT NULL,
+   created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+   summary TEXT NOT NULL,
+   PRIMARY KEY(user_id)
+);
